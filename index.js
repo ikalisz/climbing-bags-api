105009871e5b6b5aee370bca1642fb68eb02a8e3
Ian Kaliszewski <ikalisz@yahoo.com>
2020-10-05 22:09:04 -0600

    initial commit for server

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const feathers = require('@feathersjs/feathers')
+const express = require('@feathersjs/express')
+const socketio = require('@featherjs/socketio')
+
+class MessageService {
+  constructor() {
+    this.messages = []
+  }
+
+  async find() {
+    return this.messages
+  }
+
+  async create(data) {
+    const message = {
+      id: this.messages.length,
+      text: data.text,
+    }
+
+    this.messages.push(message)
+
+    return message
+  }
+}
+
+const app = express(feathers())
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(__dirname))
+
+app.configure(express.rest())
+
+app.configure(socketio())
+
+app.use('/messages', new MessageService())
+
+app.use(express.errorHandler())
+
+app.on('connection', connection => (
+  app.channel('everybody').join(connection)
+))
+
+app.listen(3030).on('listening', () => (
+  console.log('Feathers server listening on port 3030')
+))
+
+app.service('messages').create({
+  text: 'Hello world from the server',
+})
